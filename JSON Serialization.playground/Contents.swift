5e30bc4e8b5583e25b6a68ed6e9c12eff35294ff
Samet Yatmaz <sametytmz@gmail.com>
2021-01-10 12:14:57 +0300

    Json serialization was developed.

--- JSON Serialization.playground/Contents.swift
+++ JSON Serialization.playground/Contents.swift
@@ -1,3 +1,62 @@
 import UIKit
 
-var str = "Hello, playground"
+struct Customer {
+    var firstName: String
+    var lastName: String
+    var age: Int
+}
+
+extension Customer {
+    init?(dictionary: [String:Any]) {
+        guard let firstName = dictionary["firstName"] as? String,
+              let lastName = dictionary["lastName"] as? String,
+              let age = dictionary["age"] as? Int else { return nil }
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+}
+
+let json = """
+
+{
+    "firstName" : "Samet",
+    "lastName" : "Yatmaz",
+    "age" : 31
+}
+
+""".data(using: .utf8)!
+
+if let dictionary = try? JSONSerialization.jsonObject(with: json, options: .allowFragments) as? [String:Any] {
+    if let customer = Customer(dictionary: dictionary) {
+        print(customer)
+    }
+}
+
+
+let jsonList = """
+
+[
+    {
+        "firstName" : "Samet",
+        "lastName" : "Yatmaz",
+        "age" : 31
+    },
+    {
+        "firstName" : "Aykut",
+        "lastName" : "Kocaman",
+        "age" : 55
+    },
+    {
+        "firstName" : "Ridvan",
+        "lastName" : "Dilmen",
+        "age" : 58
+    }
+]
+
+""".data(using: .utf8)!
+
+if let customerList = try? JSONSerialization.jsonObject(with: jsonList, options: .allowFragments) as? [[String:Any]] {
+    let customers = customerList.map(Customer.init)
+    print(customers)
+}
