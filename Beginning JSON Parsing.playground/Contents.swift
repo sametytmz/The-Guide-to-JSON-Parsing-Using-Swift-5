7dbbb120aa24d8c4e59363e4de1e0b63de2a0bdd
Samet Yatmaz <sametytmz@gmail.com>
2021-01-10 16:41:54 +0300

    Encoding was developed.

--- Beginning JSON Parsing.playground/Contents.swift
+++ Beginning JSON Parsing.playground/Contents.swift
@@ -1,10 +1,12 @@
 import UIKit
 
-struct Customer: Decodable {
+struct Customer: Codable {
     var firstName: String
     var lastName: String
     var age: Int
-    
+}
+
+extension Customer {
     private enum ResultCodingKeys: String, CodingKey {
         case firstName
         case lastName
@@ -17,8 +19,16 @@ struct Customer: Decodable {
         self.lastName = try container.decode(String.self, forKey: .lastName)
         self.age = try container.decode(Int.self, forKey: .age)
     }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: ResultCodingKeys.self)
+        try container.encode(self.firstName, forKey: .firstName)
+        try container.encode(self.lastName, forKey: .lastName)
+        try container.encode(self.age, forKey: .age)
+    }
 }
 
+// Decoding...
 let json = """
 
 {
@@ -32,3 +42,10 @@ let json = """
 
 let customer = try! JSONDecoder().decode(Customer.self, from: json)
 print(customer)
+
+
+// Encoding...
+let customerEncodable = Customer(firstName: "Samet", lastName: "Yatmaz", age: 31)
+let encodedCustomerJSON = try! JSONEncoder().encode(customerEncodable)
+print(encodedCustomerJSON)
+print(String(data: encodedCustomerJSON, encoding: .utf8)!)
