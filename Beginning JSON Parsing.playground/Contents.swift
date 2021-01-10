e940b85cf759133f8e1d81636b219802ea8b538a
Samet Yatmaz <sametytmz@gmail.com>
2021-01-10 17:00:37 +0300

    Decoding json object array was developed.

--- Beginning JSON Parsing.playground/Contents.swift
+++ Beginning JSON Parsing.playground/Contents.swift
@@ -49,3 +49,66 @@ let customerEncodable = Customer(firstName: "Samet", lastName: "Yatmaz", age: 31
 let encodedCustomerJSON = try! JSONEncoder().encode(customerEncodable)
 print(encodedCustomerJSON)
 print(String(data: encodedCustomerJSON, encoding: .utf8)!)
+
+
+
+// Decoding Json Object Array
+
+struct Place: Decodable {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    var visited: Bool
+}
+
+extension Place {
+   private enum ResultCodingKeys: String, CodingKey {
+        case name
+        case latitude
+        case longitude
+        case visited
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ResultCodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.latitude = try container.decode(Double.self, forKey: .latitude)
+        self.longitude = try container.decode(Double.self, forKey: .longitude)
+        self.visited = try container.decode(Bool.self, forKey: .visited)
+    }
+}
+
+let jsonList = """
+
+[
+    {
+        "name" : "Costa Rica",
+        "latitude" : 23.45,
+        "longitude" : 45.23,
+        "visited" : true
+    },
+    {
+        "name" : "Puerto Rico",
+        "latitude" : 23,
+        "longitude" : 45,
+        "visited" : true
+    },
+    {
+        "name" : "Mexico City",
+        "latitude" : 23,
+        "longitude" : 45,
+        "visited" : true
+    },
+    {
+        "name" : "Iceland",
+        "latitude" : 23,
+        "longitude" : 45,
+        "visited" : false
+    }
+]
+
+""".data(using: .utf8)!
+
+let places = try! JSONDecoder().decode([Place].self, from: jsonList)
+print(places[1].name)
+print(places)
