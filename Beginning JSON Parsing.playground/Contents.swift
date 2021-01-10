45474a86ac0d8b5441e767315ef0dfddcc065411
Samet Yatmaz <sametytmz@gmail.com>
2021-01-10 16:33:13 +0300

    Decoding was developed.

--- Beginning JSON Parsing.playground/Contents.swift
+++ Beginning JSON Parsing.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var str = "Hello, playground"
+struct Customer: Decodable {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    
+    private enum ResultCodingKeys: String, CodingKey {
+        case firstName
+        case lastName
+        case age
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ResultCodingKeys.self)
+        self.firstName = try container.decode(String.self, forKey: .firstName)
+        self.lastName = try container.decode(String.self, forKey: .lastName)
+        self.age = try container.decode(Int.self, forKey: .age)
+    }
+}
+
+let json = """
+
+{
+    "firstName" : "Samet",
+    "lastName" : "Yatmaz",
+    "age" : 31
+}
+
+""".data(using: .utf8)!
+
+
+let customer = try! JSONDecoder().decode(Customer.self, from: json)
+print(customer)
